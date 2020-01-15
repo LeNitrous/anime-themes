98175c553d092ac38b947c164d3ca886606a53ab
unknown <n20gaming2000@gmail.com>
2020-01-15 19:43:16 +0800

    correctly export library

--- src/response/anime-response.ts
+++
@@ -1,9 +0,0 @@
-import { IAnimeThemeResponse } from "./theme-response";
-
-export default interface IAnimeResponse {
-    malID: number;
-    name: string;
-    year: number;
-    season: string;
-    themes: IAnimeThemeResponse[];
-}
